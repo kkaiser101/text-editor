7b0f60c689829b11674162d4b457d76fbe74ecbd
Ken Kaiser <kenkaiser19@gmail.com>
2023-03-01 16:21:11 -0700

    first upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Text Editor Starter Code
+# Text Editor
+
+## Description
+
+The purpose of this project was to get experience using progressive web applications. Whilst I was able to get the build to run on a local host, it seems to only let me do it once until giving a `npm WARN config production Use `--omit=dev` instead` error on the command line. None the less, I did my best to add the necessary code to get this build to work properly. This is a project that will be looked at again in the future in order to better understand the concepts. 
+
+## Installation
+
+- Make sure to have all the necessary packages installed when running this build from the command line!
+
+## Credits
+
+Thank you to the University of Arizona for providing me the tools and resources to gain experience in this part of web development. I am excited to take what I have learned with me as I progress through my coding journey!
+
+## License
+
+MIT License
---
+++ client/src/js/database.js
@@ -0,0 +1,49 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  
+  console.error('putDb not implemented')
+
+  const contentDb = await openDB('content', 1);
+
+  const tx = contentDb.transaction('content', 'change');
+
+  const store = tx.objectStore('content');
+
+  const request = store.add({content});
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb not implemented');
+
+  const contentDb = await openDB('content', 1);
+
+  const tx = contentDb.transaction('content', 'change');
+
+  const store = tx.objectStore('content');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+
+initdb();
